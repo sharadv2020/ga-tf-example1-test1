b288a27adbae26330c113e464164e38ed9d99a5a
sharadv07 <sharad.vidyapathy@gmail.com>
2023-12-15 14:47:47 -0500

    first commit

---
+++ providers.tf
@@ -0,0 +1,29 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.8.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+  required_version = ">= 1.7.0"
+  backend "azurerm" {
+
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
